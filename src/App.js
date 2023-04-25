8dd7bac5adfc4323e290327255a5285aa339e118
Emanuel Borges <emanuel.borges@indt.org.br>
2023-04-25 13:55:18 -0300

    zoom_in_zoom_out_zoom_reset_txt

--- src/App.js
+++ src/App.js
@@ -1,127 +1,126 @@
-import React from 'react';
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
-import PrismaZoom from 'react-prismazoom'
-
+import React, { useState , useEffect} from 'react';
 import './App.css';
-
 function App() {
+  const [originalSize, setOriginalSize] = useState(16); // assuming 16 is the original font size
+
+  const [fontSize, setFontSize] = useState(16); 
+  useEffect(() => {
+    setOriginalSize(fontSize);
+  }, []);
+
+  const zoomIn = () => {
+    setFontSize((prevSize) => prevSize + 2);
+  };
+  const zoomOut = () => {
+    setFontSize((prevSize) => (prevSize > 2 ? prevSize - 2 : prevSize));
+  };
+
+  const zoomReset = () => {
+    setFontSize(originalSize);
+  };
+
   return (
     <div className="App">
-     
-        <TransformWrapper
-          initialScale={1}
-          initialPositionX={0}
-          initialPositionY={0}
-        >
-          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-            <React.Fragment>
-              <div>
-                <button onClick={() => zoomIn()}>+</button>
-                <button onClick={() => zoomOut()}>-</button>
-                <button onClick={() => resetTransform()}>x</button>
-              </div>
-              <TransformComponent>
-        
+      <button onClick={zoomIn}>Zoom In </button>
+      <button onClick={zoomOut}>Zoom Out</button>
+      <button onClick={zoomReset}>Reset</button>
 
-              <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                <h1 class="display-4">Pricing</h1>
-                <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
-              </div>
+      <p style={{ fontSize: `${fontSize}px` }}>
+          
+          <div className="card-body">
+                  <h1 className="card-title pricing-card-title">Pricing</h1>
+                  <p className="list-unstyled mt-3 mb-4">Quickly build an effective pricing table for your potential customers with this Bootstrap example. </p>
+                  <p className="list-unstyled mt-3 mb-4">It's built with default Bootstrap components and utilities with little customization.</p>
+          </div>
 
-              <div class="container">
-                <div class="card-deck mb-3 text-center">
-                  <div class="card mb-4 box-shadow">
-                    <div class="card-header">
-                      <h4 class="my-0 font-weight-normal">Free</h4>
+                <div className="container">
+                  <div className="card-deck mb-3 text-center">
+                    <div className="card mb-4 box-shadow">
+                      <div className="card-header">
+                        <h4 className="list-unstyled mt-3 mb-4">Free</h4>
+                      </div>
+                      <div className="card-body">
+                        <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small></h1>
+                        <ul className="list-unstyled mt-3 mb-4">
+                          <li>10 users included</li>
+                          <li>2 GB of storage</li>
+                          <li>Email support</li>
+                          <li>Help center access</li>
+                        </ul>
+                        <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+                      </div>
                     </div>
-                    <div class="card-body">
-                      <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-                      <ul class="list-unstyled mt-3 mb-4">
-                        <li>10 users included</li>
-                        <li>2 GB of storage</li>
-                        <li>Email support</li>
-                        <li>Help center access</li>
-                      </ul>
-                      <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+                    <div className="card mb-4 box-shadow">
+                      <div className="card-header">
+                        <h4 className="my-0 font-weight-normal">Pro</h4>
+                      </div>
+                      <div className="card-body">
+                        <h1 className="card-title pricing-card-title">$15 <small className="text-muted">/ mo</small></h1>
+                        <ul className="list-unstyled mt-3 mb-4">
+                          <li>20 users included</li>
+                          <li>10 GB of storage</li>
+                          <li>Priority email support</li>
+                          <li>Help center access</li>
+                        </ul>
+                        <button type="button" className="btn btn-lg btn-block btn-primary">Get started</button>
+                      </div>
+                    </div>
+                    <div className="card mb-4 box-shadow">
+                      <div className="card-header">
+                        <h4 className="my-0 font-weight-normal">Enterprise</h4>
+                      </div>
+                      <div className="card-body">
+                        <h1 className="card-title pricing-card-title">$29 <small className="text-muted">/ mo</small></h1>
+                        <ul className="list-unstyled mt-3 mb-4">
+                          <li>30 users included</li>
+                          <li>15 GB of storage</li>
+                          <li>Phone and email support</li>
+                          <li>Help center access</li>
+                        </ul>
+                        <button type="button" className="btn btn-lg btn-block btn-primary">Contact us</button>
+                      </div>
                     </div>
                   </div>
-                  <div class="card mb-4 box-shadow">
-                    <div class="card-header">
-                      <h4 class="my-0 font-weight-normal">Pro</h4>
+
+                  <footer className="pt-4 my-md-5 pt-md-5 border-top">
+                    <div className="row">
+                      <div className="col-12 col-md">
+                        {/* <img className="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24"> */}
+                        <small className="d-block mb-3 text-muted">&copy; 2017-2018</small>
+                      </div>
+                      <div className="col-6 col-md">
+                        <h5>Features</h5>
+                        <ul className="list-unstyled text-small">
+                          <li><a className="text-muted" href="#">Cool stuff</a></li>
+                          <li><a className="text-muted" href="#">Random feature</a></li>
+                          <li><a className="text-muted" href="#">Team feature</a></li>
+                          <li><a className="text-muted" href="#">Stuff for developers</a></li>
+                          <li><a className="text-muted" href="#">Another one</a></li>
+                          <li><a className="text-muted" href="#">Last time</a></li>
+                        </ul>
+                      </div>
+                      <div className="col-6 col-md">
+                        <h5>Resources</h5>
+                        <ul className="list-unstyled text-small">
+                          <li><a className="text-muted" href="#">Resource</a></li>
+                          <li><a className="text-muted" href="#">Resource name</a></li>
+                          <li><a className="text-muted" href="#">Another resource</a></li>
+                          <li><a className="text-muted" href="#">Final resource</a></li>
+                        </ul>
+                      </div>
+                      <div className="col-6 col-md">
+                        <h5>About</h5>
+                        <ul className="list-unstyled text-small">
+                          <li><a className="text-muted" href="#">Team</a></li>
+                          <li><a className="text-muted" href="#">Locations</a></li>
+                          <li><a className="text-muted" href="#">Privacy</a></li>
+                          <li><a className="text-muted" href="#">Terms</a></li>
+                        </ul>
+                      </div>
                     </div>
-                    <div class="card-body">
-                      <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
-                      <ul class="list-unstyled mt-3 mb-4">
-                        <li>20 users included</li>
-                        <li>10 GB of storage</li>
-                        <li>Priority email support</li>
-                        <li>Help center access</li>
-                      </ul>
-                      <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-                    </div>
-                  </div>
-                  <div class="card mb-4 box-shadow">
-                    <div class="card-header">
-                      <h4 class="my-0 font-weight-normal">Enterprise</h4>
-                    </div>
-                    <div class="card-body">
-                      <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-                      <ul class="list-unstyled mt-3 mb-4">
-                        <li>30 users included</li>
-                        <li>15 GB of storage</li>
-                        <li>Phone and email support</li>
-                        <li>Help center access</li>
-                      </ul>
-                      <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
-                    </div>
-                  </div>
+                  </footer>
                 </div>
-
-                <footer class="pt-4 my-md-5 pt-md-5 border-top">
-                  <div class="row">
-                    <div class="col-12 col-md">
-                      {/* <img class="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24"> */}
-                      <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
-                    </div>
-                    <div class="col-6 col-md">
-                      <h5>Features</h5>
-                      <ul class="list-unstyled text-small">
-                        <li><a class="text-muted" href="#">Cool stuff</a></li>
-                        <li><a class="text-muted" href="#">Random feature</a></li>
-                        <li><a class="text-muted" href="#">Team feature</a></li>
-                        <li><a class="text-muted" href="#">Stuff for developers</a></li>
-                        <li><a class="text-muted" href="#">Another one</a></li>
-                        <li><a class="text-muted" href="#">Last time</a></li>
-                      </ul>
-                    </div>
-                    <div class="col-6 col-md">
-                      <h5>Resources</h5>
-                      <ul class="list-unstyled text-small">
-                        <li><a class="text-muted" href="#">Resource</a></li>
-                        <li><a class="text-muted" href="#">Resource name</a></li>
-                        <li><a class="text-muted" href="#">Another resource</a></li>
-                        <li><a class="text-muted" href="#">Final resource</a></li>
-                      </ul>
-                    </div>
-                    <div class="col-6 col-md">
-                      <h5>About</h5>
-                      <ul class="list-unstyled text-small">
-                        <li><a class="text-muted" href="#">Team</a></li>
-                        <li><a class="text-muted" href="#">Locations</a></li>
-                        <li><a class="text-muted" href="#">Privacy</a></li>
-                        <li><a class="text-muted" href="#">Terms</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </footer>
-              </div>
-              </TransformComponent>
-            </React.Fragment>
-          )}
-        </TransformWrapper>
-
-       
-        
+        </p>
     </div>
   );
 }
